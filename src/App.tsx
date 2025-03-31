17ccee4fa5796d204b9e3aa70b597a8b001691fd
joostkamstra <kamstra@gmail.com>
2025-03-31 06:59:58 +0200

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>Parkeerapp MVP</h1>
+      <p>Welkom! Binnenkort live parkeren...</p>
+    </div>
+  )
+}
+
+export default App
