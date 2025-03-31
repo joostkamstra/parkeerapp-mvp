beae7fa98b1d083a8e8f47406e1673852e0653e1
joostkamstra <kamstra@gmail.com>
2025-03-31 10:37:13 +0200

    Fix Vite config for proper build entry

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
+  root: '.', // root van je project
+  publicDir: 'public',
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
